41bea416304633fcb0b03ba1687ebc1f3f8e5f5b
Aaron Imbrock <aimbrock@gmail.com>
2019-07-13 18:57:51 -0700

    Challenge 4 passes

--- code-challenges/301/for-each/challenges-01.test.js
+++ code-challenges/301/for-each/challenges-01.test.js
@@ -75,7 +75,8 @@ Write a function named removeWithForEach that produces the same output as challe
 ------------------------------------------------------------------------------------------------ */
 
 const removeWithForEach = (arr, callback) => {
-  // Solution code here...
+  arr.forEach((value, i) => callback(arr[i], arr) );
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -162,7 +163,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should remove three elements from the array', () => {
     expect(removeWithForEach([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], removeOne)).toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
     expect(removeWithForEach([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], removeOne).length).toStrictEqual(7);
