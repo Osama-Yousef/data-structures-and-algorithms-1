ec0577437bb636db20d2f020dd98337daa2cf06f
Aaron Imbrock <aimbrock@gmail.com>
2019-07-24 23:47:54 -0700

    Challenge 3 passes

--- code-challenges/301/regex/challenges-04.test.js
+++ code-challenges/301/regex/challenges-04.test.js
@@ -32,9 +32,7 @@ CHALLENGE 3
 Write a function named citiesAtoJ that takes in an array of city names and uses a regular expression pattern to return a new array containing any cities that begin with the letters A through J, inclusive.
 ------------------------------------------------------------------------------------------------ */
 
-const citiesAtoJ = (arr) => {
-  // Solution code here...
-};
+const citiesAtoJ = (arr) => arr.filter(element => element.match(/^[A-J]\w+/));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
