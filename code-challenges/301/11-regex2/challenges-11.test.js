abb05385190d23719aee6865797be420e029e10b
Aaron Imbrock <aimbrock@gmail.com>
2019-08-16 23:16:10 -0700

    Challenge 3 passes

--- code-challenges/301/11-regex2/challenges-11.test.js
+++ code-challenges/301/11-regex2/challenges-11.test.js
@@ -49,9 +49,7 @@ Your function should include a single regular expression pattern that matches an
 Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
-const validatePhoneNumber = (phoneNumber) => {
-  // Solution code here...
-};
+const validatePhoneNumber = (phoneNumber) => /^(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/g.test(phoneNumber);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4 - Stretch Goal
@@ -119,7 +117,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should match the acceptable phone number formats', () => {
     expect(validatePhoneNumber('(555) 555-5555')).toBeTruthy();
     expect(validatePhoneNumber('555 555-5555')).toBeTruthy();
