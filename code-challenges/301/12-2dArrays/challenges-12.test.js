508658226f522380680b2fd987b6e70912922437
Aaron Imbrock <aimbrock@gmail.com>
2019-08-20 11:49:23 -0700

    Challenge 8 passes now

--- code-challenges/301/12-2dArrays/challenges-12.test.js
+++ code-challenges/301/12-2dArrays/challenges-12.test.js
@@ -163,9 +163,10 @@ The function should parse the string as rows and columns and compute the sum of 
 For example, excel('1,1,1\n4,4,4\n9,9,9') returns [3, 12, 27].
 ------------------------------------------------------------------------------------------------ */
 
-const excel = (str) => {
-  // Solution code here...
-};
+const excel = (str) => str.split('\n')
+  .map(x => x.split(',')
+    .map(row => Number.parseInt(row))
+    .reduce((acc, cur) => acc + cur))
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
@@ -258,7 +259,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return the total count for each row', () => {
     let result = excel('1,1,1\n4,4,4\n9,9,9');
     expect(result.length).toStrictEqual(3);
