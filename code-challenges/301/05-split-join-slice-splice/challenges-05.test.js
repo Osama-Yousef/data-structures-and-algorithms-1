133d477084a7da00f26645e6e6e5d908b2e02b24
Aaron Imbrock <aimbrock@gmail.com>
2019-08-26 12:25:37 -0700

    Challenge 5 passes

--- code-challenges/301/05-split-join-slice-splice/challenges-05.test.js
+++ code-challenges/301/05-split-join-slice-splice/challenges-05.test.js
@@ -106,9 +106,9 @@ Return a new array containing just the verbs. For example, ['Mix until evenly di
 ------------------------------------------------------------------------------------------------ */
 
 const stepActions = (recipe) => {
-  let result = [];
-  // Solution code here...
-  return result;
+  let verbs = [];
+  recipe.steps.forEach(items => verbs.push(items.split(' ')[0]));
+  return verbs;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -230,7 +230,7 @@ xdescribe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return a list of recipe steps', () => {
     expect(stepActions(gruffaloCrumble)).toStrictEqual(['Pre-heat', 'De-prickle', 'Sprinkle', 'Mix', 'Grease', 'Combine', 'Fold', 'Spread', 'Bake']);
     expect(stepActions(gruffaloCrumble).length).toStrictEqual(9);
