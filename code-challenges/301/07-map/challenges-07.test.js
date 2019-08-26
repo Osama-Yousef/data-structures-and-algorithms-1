1e80417850910ea5b79db29636f95e937aa9800f
Aaron Imbrock <aimbrock@gmail.com>
2019-08-26 15:45:42 -0700

    Challenge 3 passes

--- code-challenges/301/07-map/challenges-07.test.js
+++ code-challenges/301/07-map/challenges-07.test.js
@@ -38,9 +38,7 @@ CHALLENGE 3
 Write a function named mapTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1 and your forEachTwoToThe function from challenge 2, but uses map instead of a for loop or forEach.
 ------------------------------------------------------------------------------------------------ */
 
-const mapTwoToThe = (arr) => {
-  // Solution code here...
-};
+const mapTwoToThe = (arr) => arr.map(exp => Math.pow(2, exp));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -194,7 +192,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(mapTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(mapTwoToThe([0, 4, 5]).length).toStrictEqual(3);
