ddd1f2c2313d7caf8f563140647c895dfeb3960c
Aaron Imbrock <aimbrock@gmail.com>
2019-08-26 18:24:20 -0700

    Challenge 5 passes

--- code-challenges/301/10-chaining/challenges-10.test.js
+++ code-challenges/301/10-chaining/challenges-10.test.js
@@ -44,7 +44,6 @@ const divisibleByFiveTwoToThePower = (input) =>
     }).map(i => Math.pow(2, i))
   );
 
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
 
@@ -107,9 +106,13 @@ let starWarsData = [{
   gender: 'female'
 }];
 
-let findMaleAndFemale = (data) => {
-  // Solution code here...
-};
+let findMaleAndFemale = (data) =>
+  data.filter(char => (
+    (char.gender === 'male' || char.gender === 'female')))
+    .reduce((acc, val) => {
+      acc.push(val.name)
+      return acc;
+    }, []).join(' and ');
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
@@ -117,8 +120,8 @@ CHALLENGE 5
 Write a function named findShortest that, given the Star Wars data from Challenge 6, uses any combination of filter, map and reduce to return the name of the shortest character.
 ------------------------------------------------------------------------------------------------ */
 
-let findShortest = (data) => data.reduce((acc,val)=>{
-  if(parseInt(val.height) < parseInt(acc.height)){ acc = val}
+let findShortest = (data) => data.reduce((acc,val) => {
+  if (parseInt(val.height) < parseInt(acc.height)) {acc = val}
   return acc
 }).name
 
@@ -167,7 +170,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return only characters that are male or female', () => {
     expect(findMaleAndFemale(starWarsData)).toStrictEqual('Luke Skywalker and Darth Vader and Leia Organa');
     expect(findMaleAndFemale([{ name: 'person', gender: 'female' }, { gender: 'lol' }, { name: 'persontwo', gender: 'male' }])).toStrictEqual('person and persontwo');
