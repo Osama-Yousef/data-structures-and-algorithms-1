106285eef3779ff6eee681775522c0602898fab7
Aaron Imbrock <aimbrock@gmail.com>
2019-12-04 22:44:18 -0800

    Added tests. All tests passing

--- challenges/03-array-binary-search/array_binary_search.py
+++ challenges/03-array-binary-search/array_binary_search.py
@@ -1,22 +1,23 @@
 def binary_search(arr, target):
     left_idx = 0
     right_idx = len(arr) - 1
-    outcome = -1
 
-    if arr != []:
-        while (left_idx != right_idx) and (outcome != -1):
+    if len(arr) > 1 and arr[right_idx] > target:
+        while (left_idx != right_idx):
             mid_idx = ((left_idx + right_idx ) // 2)
             if target < arr[mid_idx]:
                 mid_idx = right_idx 
             elif target > arr[mid_idx]:
                 mid_idx = left_idx
             else:
-                break
-        
-        
-            if arr[mid_idx] == target:
-                outcome = mid_idx
-            else:
                 outcome = -1
+                break
+       
+        if arr[mid_idx] == target:
+            outcome = mid_idx
+        else:
+            outcome = -1
+    else:
+        outcome = -1
 
     return outcome
--- challenges/03-array-binary-search/test_binary_search.py
+++ challenges/03-array-binary-search/test_binary_search.py
@@ -8,3 +8,26 @@ def test_binary_search_null():
     expected = -1
     assert expected == actual
 
+def test_binary_search_even():
+    input_arr = [4,8,15,16,23,42]
+    input_target = 15
+
+    actual = binary_search(input_arr, input_target)
+    expected = 2
+    assert expected == actual
+
+def test_binary_search_odd():
+    input_arr = [4,8,15,16,23,42]
+    input_target = 15
+
+    actual = binary_search(input_arr, input_target)
+    expected = 2
+    assert expected == actual
+
+def test_binary_search_not_found():
+    input_arr = [11,22,33,44,55,66,77]
+    input_target = 90
+
+    actual = binary_search(input_arr, input_target)
+    expected = -1
+    assert expected == actual
