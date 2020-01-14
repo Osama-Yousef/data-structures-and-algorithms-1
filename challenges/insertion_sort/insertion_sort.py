fb5dd5a228718a0e0f28f09d01160e34ca12aea2
Aaron Imbrock <aimbrock@gmail.com>
2020-01-13 20:20:40 -0800

    Finished BLOG

--- challenges/insertion_sort/insertion_sort.py
+++ challenges/insertion_sort/insertion_sort.py
@@ -6,8 +6,12 @@ def insertion_sort(array):
 
         while position >= 0 and temp < array[position]:
             array[position + 1] = array[position]
-            position = position - 1
+            position -= 1
 
         array[position + 1] = temp
+        print(array)
 
-    return array
+
+arr = [20,18,12,8,5,-2]
+
+insertion_sort(arr)
