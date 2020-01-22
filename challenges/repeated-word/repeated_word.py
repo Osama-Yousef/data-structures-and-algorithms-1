561cea34b5b7069e37d63f5c02459a6c9a4edba4
Aaron Imbrock <aimbrock@gmail.com>
2020-01-22 15:07:46 -0800

    Added repeated word challenge files

--- challenges/repeated-word/repeated_word.py
+++ challenges/repeated-word/repeated_word.py
@@ -1,4 +1,4 @@
-
+import re
 
 """
 - Write a function that accepts a lengthy string parameter.
@@ -10,3 +10,22 @@ return the first word to occur more than once in that provided string.
 """
 
 def repeated_word(string):
+    '''
+    Takes a string and returns first word to occur more
+    than once in that provided string.
+    '''
+    regex = re.compile(r'[a-z]')
+    unique = set()
+    phrase = string.lower().split(' ')
+    word_count = 0
+
+    for word in phrase:
+        word_count += 1
+        if word in unique:
+            return word, word_count
+        unique.add(word)
+    return None
+
+if __name__ == "__main__":
+    test_word = 'asdf8u3ksu1234'
+    repeated_word(test_word)
