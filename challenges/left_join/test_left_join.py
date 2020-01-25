072ba36bddd27ce3b7748f9763231076b1625e4b
Aaron Imbrock <aimbrock@gmail.com>
2020-01-25 14:25:35 -0800

    Added in tests for this challenge

--- challenges/left_join/test_left_join.py
+++ challenges/left_join/test_left_join.py
@@ -1,33 +1,50 @@
-# import pytest
+import pytest
 from left_join import join
 
 @pytest.fixture
-city_population = {
-    "New York City" : 8550405,
-    "Los Angeles" : 3971883,
-    "Toronto" : 2731571,
-    "Chicago" : 2720546,
-    "Houston" : 2296224,
-    "Montreal" : 1704694,
-    "Calgary" : 1239220,
-    "Vancouver" : 631486,
-    "Boston" : 667137,
-    "Bellevue" : 273983,
-    "Seattle" : 8292211111,
-}
+def city_population():
+
+    city_population = {
+        "New York City" : 8550405,
+        "Los Angeles" : 3971883,
+        "Toronto" : 2731571,
+        "Chicago" : 2720546,
+        "Houston" : 2296224,
+        "Montreal" : 1704694,
+        "Calgary" : 1239220,
+        "Vancouver" : 631486,
+        "Boston" : 667137,
+        "Bellevue" : 273983,
+        "Seattle" : 8292211111,
+    }
+
+    return city_population
 
 @pytest.fixture
-city_walkability_score = {
-    "New York City" : 88,
-    "Los Angeles" : 44,
-    "Toronto" : 82,
-    "Chicago" : 78,
-    "Houston" : 4,
-    "Montreal" : 99,
-    "Calgary" : 33,
-    "Vancouver" : 93,
-    "Boston" : 21,
-    "Vancouver" : 95,
-    "Auburn" : 0,
-}
+def city_walkability_score():
+    
+    city_walkability_score = {
+        "New York City" : 88,
+        "Los Angeles" : 44,
+        "Toronto" : 82,
+        "Chicago" : 78,
+        "Houston" : 4,
+        "Montreal" : 99,
+        "Calgary" : 33,
+        "Vancouver" : 93,
+        "Boston" : 21,
+        "Vancouver" : 95,
+        "Auburn" : 0,
+    }
 
+    return city_walkability_score
+
+def test_join_none_in_table_1():
+    '''join handles missing keys correctly in table_1'''
+    pass
+
+def test_join_none_in_table_2():
+    '''join handles missing keys correctly in table_2'''
+    pass
+
+def test_
