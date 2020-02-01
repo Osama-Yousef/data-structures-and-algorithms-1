13198e068ea74f3cf701631e9ef4457dfb9a3933
Aaron Imbrock <aimbrock@gmail.com>
2020-02-01 15:54:05 -0800

    add method test passes

--- challenges/hash_tables/test_hash_table.py
+++ challenges/hash_tables/test_hash_table.py
@@ -1,9 +1,8 @@
-# Adding a key/value to your hashtable results in the value being in the data structure
 # Retrieving based on a key returns the value stored
 # Successfully returns null for a key that does not exist in the hashtable
 # Successfully handle a collision within the hashtable
 # Successfully retrieve a value from a bucket within the hashtable that has a collision
-# Successfully hash a key to an in-range value
+
 
 import pytest
 from hash_table import HashTable
@@ -14,3 +13,15 @@ def test_hash_is_deterministic():
     actual = ht.hash('dog')
     assert actual == ht.hash('dog')
     assert 0 <= actual < 1024
+
+def test_hash_add():
+    '''Test that adding a key/value to your hashtable results in the value being in the data structure'''
+    ht = HashTable()
+    idx = ht.hash('dog')
+    ht.add('dog','what ever you want')
+    ll = ht.buckets[idx]
+    key = ll.head._data.get('key')
+    value = ll.head._data.get('value')
+    assert 'dog' == key
+    assert 'what ever you want' == value
+
